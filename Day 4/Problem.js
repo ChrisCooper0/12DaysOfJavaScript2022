6c3107f998acefb2ebbe4ddd62d58600a265f198
ChrisCooper0 <christopher.cooper@mail.com>
2022-12-03 15:56:17 +0000

    Day 4

---
+++ Day 4/Problem.js
@@ -0,0 +1,8 @@
+// Write a function `isDivBy4` that accepts a number as an argument. The function should return a boolean indicating whether or not the number is divisible by 4
+
+const isDivBy4 = (num) => {};
+
+console.log(isDivBy4(16)); // true
+console.log(isDivBy4(32)); // true
+console.log(isDivBy4(18)); // false
+console.log(isDivBy4(7)); // false
