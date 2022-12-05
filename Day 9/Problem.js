8e448a7cc4fd3a2c164bcaf3ade7ff9048105a69
ChrisCooper0 <christopher.cooper@mail.com>
2022-12-05 20:29:39 +0000

    Day 9

--- Day 9/Problem.js
+++ Day 9/Problem.js
@@ -1,6 +1,6 @@
-// Write a function `commonElements` that accepts two arrays as arguments. The function should return a new array containing the elements that are found in both of the input arrays
+// Write a function `zipper` that accepts two arrays as arguments. The function should return a 2D array containing pairs of elements at the same indices. You can assume that the arrays have the same length
 
-const commonElements = (arr1, arr2) => {};
+const zipper = (arr1, arr2) => {};
 
-console.log(commonElements(["a", "b", "c", "d"], ["c", "d", "e", "f"])); // ['c', 'd']
-console.log(commonElements([1, 4, 5, 7], [5, 8, 3, 1])); // [1, 5]
+console.log(zipper([1, 2, 3], ["a", "b", "c"]));
+// [ [ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ] ]
--- Day 9/Solution.js
+++ Day 9/Solution.js
@@ -1,16 +1,14 @@
-// Write a function `commonElements` that accepts two arrays as arguments. The function should return a new array containing the elements that are found in both of the input arrays
+// Write a function `zipper` that accepts two arrays as arguments. The function should return a 2D array containing pairs of elements at the same indices. You can assume that the arrays have the same length
 
-const commonElements = (arr1, arr2) => {
-  let commonArr = [];
+const zipper = (arr1, arr2) => {
+  let zippedArr = [];
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr2.includes(arr1[i])) {
-      commonArr.push(arr1[i]);
-    }
+    zippedArr.push([arr1[i], arr2[i]]);
   }
 
-  return commonArr;
+  return zippedArr;
 };
 
-console.log(commonElements(["a", "b", "c", "d"], ["c", "d", "e", "f"])); // ['c', 'd']
-console.log(commonElements([1, 4, 5, 7], [5, 8, 3, 1])); // [1, 5]
+console.log(zipper([1, 2, 3], ["a", "b", "c"]));
+// [ [ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ] ]
